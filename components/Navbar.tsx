9ac98d737222212154854a2479ce14eaf72f0f72
BDunnCode <BDunnCode@github.com>
2024-02-19 00:08:44 -0600

    Basic structure and layout for UI pretty much done. A matter of filling in components now

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,6 +1,7 @@
 import Image from "next/image"
 import Link from "next/link"
 import { NAV_LINKS } from "@/constants"
+import Button from "./Button"
 
 const Navbar = () => {
   return (
@@ -19,12 +20,23 @@ const Navbar = () => {
       </ul>
 
 
-      {/*Login Button Not sure if this will use a link tag or button tag , but it will definitely have a little person icon inside and the world login*/}
+      <div className="lg:flexCenter hidden">
+          <Button
+            type="button"
+            title="Login"
+            icon="/user.svg"
+            variant="btn_dark_green"
+          />
+      </div>  
 
-      
-      {/* After that it gets made reactive, though much of that has been done already with the way he styled the css containers. 
-      There needs to be some sort of ternary operator logic happening to determine whether or to the hamburger shows, and then things get restyled 
-      according to screensize somehow*/}
+      <Image 
+        src="menu.svg"
+        alt="menu"
+        width={32}
+        height={32}
+        className="inline-block cursor-pointer lg:hidden"
+      />
+
     </nav>
   )
 }
