b04ed528625f9a9ba66eee5918ca64df401fda30
BDunnCode <BDunnCode@github.com>
2024-02-18 23:37:55 -0600

    navbar almost done

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,14 +1,27 @@
+import Image from "next/image"
 import Link from "next/link"
+import { NAV_LINKS } from "@/constants"
 
 const Navbar = () => {
   return (
-    <nav className="border-2 border-red-500 flexBetween max-container
+    <nav className="flexBetween max-container
     padding-container relative z-30 py-5">
       <Link href="/">
-        Home
+        <Image src="hilink-logo.svg" alt="logo" height={29} width={74} />
       </Link>
-      {/* Nav Links Here. Use map, make five link tags using an array. You should render 5 li items inside of a ul */}
+      <ul className="hidden h-full gap-12 lg:flex">
+        {NAV_LINKS.map((link) => (
+          <Link href={link.href} key={link.key}
+          className="regular-16 text-gray-50 flexCenter cursor-point pb-1.5 transition-all hover:font-bold">
+            {link.label}
+          </Link>
+        ))}
+      </ul>
+
+
       {/*Login Button Not sure if this will use a link tag or button tag , but it will definitely have a little person icon inside and the world login*/}
+
+      
       {/* After that it gets made reactive, though much of that has been done already with the way he styled the css containers. 
       There needs to be some sort of ternary operator logic happening to determine whether or to the hamburger shows, and then things get restyled 
       according to screensize somehow*/}
