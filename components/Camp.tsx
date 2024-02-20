2029ceea772ba46cc1d35a042423ac60c62ab86d
BDunnCode <BDunnCode@github.com>
2024-02-19 21:04:40 -0600

    Camp Section done

--- components/Camp.tsx
+++ components/Camp.tsx
@@ -53,9 +53,6 @@ const CampSite = ({ backgroundImage, title, subtitle, peopleJoined}:
 const Camp = () => {
   return (
     <section className="2xl:max-container relative flex flex-col py-10 lg:mb-10 lg:py-20 xl:mb-20">
-      {/* div containing scrolling images*/}
-        {/* div containing image 1*/}
-          {/* text and images, with transparent bg opacity? */}
       <div className="hide-scrollbar flex h-[340px] w-full items-start
       justify-start gap-8 overflow-x-auto lg:h-[400px] xl:h-[640px]">
         <CampSite
@@ -71,11 +68,25 @@ const Camp = () => {
           peopleJoined="50+ Joined"
          />
       </div>
-        {/* div containing image 2*/}
-          {/* text and images, with transparent bg opacity? */}
-          {/* The images are using z-index for that effect */}
-          {/* Container padding w/ Smaller Gap */}
-        {/* Absolutely positioned div on top of other 2 */}
+
+      <div className="flexEnd mt-10 px-6 lg:-mt-60 lg:mr-6"> 
+          <div className="bg-green-50 p-8 lg:max-w-[500px] xl:max-w-[734px] xl:rounded-5xl xl:px-16 xl:py-20 relative w-full overflow-hidden rounded-3xl ">
+            <h2 className="regular-24 md:regular-32 2xl:regular-64 text-white capitalize">
+              <strong>Feeling Lost</strong> and not knowing the way?
+            </h2>
+            <p className="regular-14 xl:regular-16 mt-5 text-white">
+              Starting from the anxiety of the climbers when visiting a new climbing location, the possibility of getting lost is very large. 
+              That's why we are here for those of you who want to start an adventure            
+            </p>
+            <Image
+              src="/quote.svg"
+              alt="camp-2"
+              width={186}
+              height={219}
+              className="camp-quote"
+            />
+          </div>
+      </div>
     </section>
   )
 }
